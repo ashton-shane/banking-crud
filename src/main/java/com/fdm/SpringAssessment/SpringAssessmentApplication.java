452d44aae950c1fcd5ad06e968e911d3b0d08785
Ashton Shane <ashton.shane.dev@gmail.com>
2026-03-30 16:32:35 +0800

    first commit

---
+++ src/main/java/com/fdm/SpringAssessment/SpringAssessmentApplication.java
@@ -0,0 +1,12 @@
+package com.fdm.SpringAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssessmentApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssessmentApplication.class, args);
+	}
+
+}
