b7042293dc2132d4ac6ccadc81757ef92c808313
Ashton Shane <ashton.shane.dev@gmail.com>
2026-04-03 19:38:03 +0800

    add react frontend structure to projecT

---
+++ frontend-react/src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+// this is just a config file to make App.js a child of BrowserRouter
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
