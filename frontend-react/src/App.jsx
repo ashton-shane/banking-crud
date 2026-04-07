66dd4c078fda231c79ec2ce968f2abff4eef7848
Ashton Shane <ashton.shane.dev@gmail.com>
2026-04-07 16:03:36 +0800

    Revert "fix accounts and customers display list and components"
    This reverts commit dd354d9e753fed1fda78fb7bd7f8bfb94e3143a6.

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -3,6 +3,11 @@ import Form from "./components/Form";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
+import { Routes, Route} from "react-router-dom";
+import FindById from "./components/FindById";
+import FindAccount from "./pages/FindAccount";
+import FindCustomer from "./pages/FindCustomer";
+
 function App() {
   return (
     <div className="app">
@@ -10,6 +15,10 @@ function App() {
 
       <div className="app-body">
         <Sidebar />
+        <Routes>
+            <Route path="/find-customer" element={<FindCustomer />}></Route>
+            <Route path="/find-account" element={<FindAccount />}></Route>
+        </Routes>
         <Form />
       </div>
     </div>
